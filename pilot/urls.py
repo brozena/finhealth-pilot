f53eebd06ce1d9ce55eb7cbfe0ebcd026793a6f3
Jeff Brozena <jeff@brozena.net>
2024-07-05 03:51:43 -0400

    PID submission renders link flow #9

--- pilot/urls.py
+++ pilot/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='pilot/index.html')),
+    path('', views.index, name='index'), 
     path('transactions', TemplateView.as_view(template_name='pilot/transactions.html')),
     path('authentication/', include('authentication.urls')),
     path('balance/', include('balance.urls')),
     path('transactions/', include('transaction.urls')),
-    path('get_pid/', views.get_pid, name='get_pid'),
+    path('pid/', views.pid, name='pid'),
 ]
--- pilot/views.py
+++ pilot/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
 
-from pilot.models import PID
 from pilot.forms import PIDForm
 
-@csrf_exempt
-def pid(request):
-    f = PIDForm(request.POST)
-    if f.is_valid():
-        f.save()
+def index(request):
+    form = PIDForm()
+    return render(request, 'pilot/index.html', {"form": form})
 
-    return redirect('/')
+@csrf_protect
+def pid(request):
+    if request.method == "POST":
+        form = PIDForm(request.POST)
+        
+        if form.is_valid():
+            participant_id = form.cleaned_data['participant_id']
+            form.save()
+        
+    return render(request, 'pilot/link.html', {"form": form})
