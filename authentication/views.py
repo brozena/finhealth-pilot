0332f728401807be3ba2d775719ea33b2e3dfa6c
Jeff Brozena <jeff@brozena.net>
2024-07-05 04:44:15 -0400

    edit auth models to tie PID #5

--- authentication/views.py
+++ authentication/views.py
@@ -32,14 +32,11 @@ from plaid.model.products import Products
 from plaid.model.country_code import CountryCode
 from .plaid_config import PlaidConfig
 
-from authentication.models import PlaidToken
+from authentication.models import PlaidItem
+from pilot.models import PID
 
 plaid_config = PlaidConfig(plaid.Environment.Sandbox)
 client = plaid_config.client()
-
-def index(request):
-    context = {}
-    return render(request, "index.html", context)
 
 @api_view(['POST'])
 async def create_link_token(request):
@@ -64,11 +61,40 @@ async def exchange_public_token(request):
     try:
         public_token = json.loads(request.body.decode('utf-8'))['public_token']
         plaid_request = ItemPublicTokenExchangeRequest(public_token=public_token)
+
         response = client.item_public_token_exchange(plaid_request)
         access_token = response['access_token']
         item_id = response['item_id']
+
         os.environ['ACCESS_TOKEN'] = access_token
         os.environ['ITEM_ID'] = item_id
+
+        #pid = PID.objects.get(id=
+        plaid_item = None
+
+        try:
+            plaid_item = PlaidItem.get(item_id=item_id)
+        except:
+            new_plaid_item = PlaidItem(pid=participant_id, access_token=access_token, item_id=item_id)
+            new_plaid_item.save()
+            plaid_item = user.plaiditem_set.get(item_id=item_id)
+
+        for account in accounts:
+            try:
+                existing_acct = user.account_set.get(plaid_account_id=account['account_id'])
+                continue
+            except:
+                new_acct = Account()
+                new_acct.plaid_account_id = account['id']
+                new_acct.mask = account['mask']
+                new_acct.name = account['name']
+                new_acct.subtype = account['subtype']
+                new_acct.account_type = account['type']
+                new_acct.user = user
+                new_acct.item = plaid_item
+                new_acct.save()
+
+
         return JsonResponse(response.to_dict())
     except ApiException as e:
         return json.dumps(e.body)
