ec3dc8e342fbf3a98cc5de4deb58ff768778bee8
Jeff Brozena <jeff@brozena.net>
2024-07-08 14:58:18 -0400

    #5 saner models between apps

--- authentication/models.py
+++ authentication/models.py
@@ -3,10 +3,11 @@ from django.conf import settings
 
 from pilot.models import PID
 
+
 class PlaidItem(models.Model):
+    participant_id = models.ForeignKey(PID, on_delete=models.CASCADE)
     access_token = models.CharField(max_length=64, default='')
     item_id = models.CharField(max_length=200, unique=True)
-    participant_id = models.ForeignKey(PID, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('access_token', 'participant_id')
