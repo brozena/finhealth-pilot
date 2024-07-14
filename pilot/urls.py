09e81e23273101756fd73dcf3ad84c7a8195e62e
Jeff Brozena <jeff@brozena.net>
2024-07-14 14:41:08 -0400

    add thanks, fix db saves, remove item at end

--- pilot/urls.py
+++ pilot/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'), 
     path('create_user/', views.create_user, name='create_user'),
     path('get_transactions/', views.get_transactions, name='get_transactions'),
-    path('link_account', views.link_account, name='link-account'),
     path('create_link_token', views.create_link_token, name='create-link-token'),
     path('get_access_token', views.get_access_token, name='get-access-token'),
     path('create_user', views.create_user, name='create-user'),
+    path('thanks/', TemplateView.as_view(template_name='thanks.html')) 
 ]
--- pilot/views.py
+++ pilot/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.views.decorators.csrf import csrf_exempt
 
-import datetime
+import datetime as dt
 from dateutil.relativedelta import relativedelta
 
 from django.http import JsonResponse
@@ -31,9 +31,9 @@ from plaid.api import plaid_api
 from plaid.model.link_token_create_request import LinkTokenCreateRequest
 from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
 from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
-from plaid.model.accounts_get_request import AccountsGetRequest
-from plaid.model.accounts_get_request_options import AccountsGetRequestOptions
+from plaid.model.item_remove_request import ItemRemoveRequest
 from plaid.model.transactions_get_request import TransactionsGetRequest
+from plaid.model.transactions_get_request_options import TransactionsGetRequestOptions
 from plaid.model.products import Products
 from plaid.model.country_code import CountryCode
 from .plaid_config import PlaidConfig
@@ -46,19 +46,19 @@ client = plaid_config.client()
 def index(request):
     return render(request, 'pilot/index.html')
 
+
 @csrf_exempt
 def create_user(request):
     username=request.POST['username']
     user = User.objects.create(username=username)
     user.save()
+
+    # apply .filter() to avoid setting a password
     user = User.objects.filter(username=username).get()
     login(request, user)
+
     return render(request, 'pilot/link.html')
 
-
-def link_account(request):
-    context = {}
-    return render(request, 'pilot/link.html', context)
 
 @csrf_exempt
 def create_link_token(request):
@@ -77,6 +77,7 @@ def create_link_token(request):
     plaid_response = client.link_token_create(plaid_request)
     return JsonResponse(plaid_response.to_dict())
 
+
 @csrf_exempt
 def get_access_token(request):
     user = request.user
@@ -88,40 +89,14 @@ def get_access_token(request):
     exchange_response = client.item_public_token_exchange(exchange_request) 
     item_id = exchange_response['item_id']
     access_token = exchange_response['access_token']
-    account_request = AccountsGetRequest(access_token=access_token)
-    account_response = client.accounts_get(account_request)
-    accounts = account_response['accounts']
 
-    user = request.user
     plaid_item = None
+    new_plaid_item = PlaidItem(user=user, access_token=access_token, item_id=item_id)
+    new_plaid_item.save()
+    plaid_item = user.plaiditem_set.get(item_id=item_id)
 
-    try:
-        plaid_item = user.plaiditem_set.get(item_id=item_id)
-    except:
-        new_plaid_item = PlaidItem(user=user, access_token=access_token, item_id=item_id)
-        new_plaid_item.save()
-        plaid_item = user.plaiditem_set.get(item_id=item_id)
+    return JsonResponse(exchange_response.to_dict())
 
-    for account in accounts:
-        try:
-            existing_acct = user.account_set.get(plaid_account_id=account['account_id'])
-            continue
-        except:
-            new_acct = Account()
-            new_acct.plaid_account_id = account['account_id']
-            new_acct.mask = account['mask']
-            new_acct.name = account['name']
-            new_acct.subtype = account['subtype']
-            new_acct.account_type = account['type']
-            new_acct.user = user
-            new_acct.item = plaid_item
-            new_acct.save()
-
-        # Pretty printing in development
-        json.dumps(exchange_response, sort_keys=True, indent=4)
-        print(exchange_response)
-
-    return redirect('/')
 
 def get_transactions(request):
     user = request.user
@@ -129,84 +104,57 @@ def get_transactions(request):
     transactions = []
     plaid_items = user.plaiditem_set.all()
 
-    start_date = (datetime.date.today() - relativedelta(months=24))
-    end_date = datetime.date.today()
-
     for item in plaid_items:
-        try:
-            access_token = item.access_token
+        access_token = item.access_token
 
-            plaid_request = TransactionsGetRequest(access_token=access_token,
-                                start_date=(datetime.date.today() - relativedelta(months=24)),
-                                end_date=datetime.date.today(),)
+        request = TransactionsGetRequest(access_token=access_token,
+                            start_date=(dt.date.today() - relativedelta(months=24)),
+                            end_date=dt.date.today(), options=TransactionsGetRequestOptions(
+                                include_original_description=True)
+                            )
 
-            transaction_response = client.transactions_get(plaid_request)
-            transactions = transaction_response['transactions']
+        response = client.transactions_get(request)
+        transactions = response['transactions']
+        accounts = response['accounts']
 
-            account_request = AccountsGetRequest(access_token=access_token)
-            account_response = client.accounts_get(account_request)
-            accounts = account_response['accounts']
+        error = None
 
-            error = None
+        for account in accounts:
+            new_acct = Account()
+            new_acct.plaid_account_id = account['account_id']
+            new_acct.balances = account['balances']['current']
+            new_acct.mask = account['mask']
+            new_acct.name = account['name']
+            new_acct.official_name = account['official_name']
+            new_acct.subtype = account['subtype']
+            new_acct.account_type = account['type']
+            new_acct.user = user
+            new_acct.save()
 
-            for account in accounts:
-                try:
-                    existing_acct = user.account_set.get(plaid_account_id=account['account_id'])
-                    continue
-                except:
-                    new_acct = Account()
-                    new_acct.plaid_account_id = account['account_id']
-                    new_acct.balances = account['balances']
-                    new_acct.mask = account['mask']
-                    new_acct.name = account['name']
-                    new_acct.official_name = account['official_name']
-                    new_acct.subtype = account['subtype']
-                    new_acct.account_type = account['type']
-                    new_acct.user = user
-                    new_acct.save()
+        for transaction in transactions:
+            new_trans = Transaction()
+            new_trans.account = user.account_set.get(plaid_account_id=transaction['account_id']) 
+            new_trans.account_owner = transaction['account_owner']
+            new_trans.amount = transaction['amount']
+            new_trans.authorized_date = transaction['authorized_date']
+            new_trans.category = transaction['category']
+            new_trans.category_id = transaction['category_id']
+            new_trans.date = dt.datetime.strftime(transaction['date'], '%Y-%m-%d')
+            new_trans.iso_currency_code = transaction['iso_currency_code']
+            new_trans.merchant_name = transaction['merchant_name']
+            new_trans.name = transaction['name']
+            new_trans.payment_channel = transaction['payment_channel']
+            new_trans.original_description = transaction['original_description']
+            new_trans.pending = transaction['pending']
+            new_trans.pending_transaction_id = transaction['pending_transaction_id']
+            new_trans.transaction_code = transaction['transaction_code']
+            new_trans.transaction_id = transaction['transaction_id']
+            new_trans.transaction_type = transaction['transaction_type']
+            new_trans.unofficial_currency_code = transaction['unofficial_currency_code']
+            new_trans.user = user
+            new_trans.save()
 
-            while len(transactions) < transaction_response['total_transactions']:
-                plaid_request = TransactionsGetRequest(access_token,
-                                    start_date=(datetime.date.today() - relativedelta(months=24)),
-                                    end_date=datetime.date.today(),)
+        request = ItemRemoveRequest(access_token=access_token)
+        response = client.item_remove(request)
 
-                transaction_response = client.transactions_get(plaid_request)
-                item_id = exchange_response['item_id']
-
-                transactions.extend(response['transactions'])
-            
-
-            for transaction in transactions:
-                try:
-                    existing_trans = user.transaction_set.get(transaction_id=transaction['transaction_id'])
-                    existing_trans.save()
-                    continue
-                except Transaction.DoesNotExist:
-                    new_trans = Transaction()
-                    new_trans.account = user.account_set.get(plaid_account_id=transaction['account_id'])
-                    new_trans.account_owner = transaction['account_owner']
-                    new_trans.amount = transaction['amount']
-                    new_trans.authorized_date = transaction['authorized_date']
-
-                    new_trans.category = transaction['category']
-                    new_trans.category_id = transaction['category_id']
-                    new_trans.date = datetime.datetime.strptime(transaction['date'], '%Y-%m-%d')
-                    new_trans.iso_currency_code = transaction['iso_currency_code']
-                    new_trans.merchant_name = transaction['merchant_name']
-                    new_trans.name = transaction['name']
-                    new_trans.payment_meta = transaction['payment_meta']
-                    new_trans.payment_channel = transaction['payment_channel']
-                    new_trans.pending = transaction['pending']
-                    new_trans.pending_transaction_id = transaction['pending_transaction_id']
-                    new_trans.transaction_code = transaction['transaction_code']
-                    new_trans.transaction_id = transaction['transaction_id']
-                    new_trans.transaction_type = transaction['transaction_type']
-                    new_trans.unofficial_currency_code = transaction['unofficial_currency_code']
-                    new_trans.user = user
-                    new_trans.save()
-        except Exception as e:
-                print(e)
-                # error = {'display_message': 'You need to link your account.' }
-        #json.dumps(transactions, sort_keys=True, indent=4)
-
-    return render(request, 'pilot/transactions.html', {'transactions': transactions})
+    return render(None, 'pilot/thanks.html')
